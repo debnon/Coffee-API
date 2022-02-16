d1cc5a9d89646c941d67fd4fe6bdec8fce1a38ff
Ben Donaghy <bendonaghy321@gmail.com>
2022-02-16 10:27:39 +0000

    added HomeController to get test passing, and SpringBootApplication starting point and testing file

---
+++ src/test/java/apilab/ApilabApplicationTests.java
@@ -0,0 +1,14 @@
+package apilab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
